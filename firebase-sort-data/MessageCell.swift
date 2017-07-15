0993f14ec933f6b617aaf4f0cb31280703b68ecc
Matt Tripodi <mtripodi1027@gmail.com>
2017-07-15 15:54:29 -0400

    Setup UI

---
+++ firebase-sort-data/MessageCell.swift
@@ -0,0 +1,20 @@
+//
+//  MessageCell.swift
+//  firebase-sort-data
+//
+//  Created by Matt Tripodi on 7/15/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+	
+	// IBOutlets 
+	@IBOutlet weak var msgTextLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- firebase-sort-data/ViewController.swift
+++ firebase-sort-data/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	// IBOutlets 
+	@IBOutlet weak var msgField: UITextField!
+	@IBOutlet weak var tableView: UITableView!
+	
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	// IBActions 
+	@IBAction func sendBtnTapped(_ sender: Any) {
 	}
-
-
+	
 }
 
